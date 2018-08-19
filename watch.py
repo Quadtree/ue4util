216d6ed83a7fa3c9728b5a66b492eb1317839755
Quadtree <quadtree@gmail.com>
2018-08-19 00:34:01 -0700

    New watch added

---
+++ watch.py
@@ -0,0 +1,72 @@
+import time
+import sys
+import os
+import re
+import pickle
+import shutil
+import tempfile
+import depfinder
+import headergen
+import curprj
+import logging
+import json
+import subprocess
+
+def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    project_file_name = None
+
+    for f in os.listdir('.'):
+        if f.endswith('.uproject'):
+            project_file_name = f
+            logging.info(f)
+
+    project_file_contents = None
+
+    with open(project_file_name) as f:
+        project_file_contents = json.load(f)
+
+    engine_version = project_file_contents['EngineAssociation']
+
+    logging.info(f'engine_version={engine_version}')
+
+    out = subprocess.check_output(['reg', 'query', f'HKLM\\SOFTWARE\\EpicGames\\Unreal Engine\\{engine_version}'])
+
+    engine_path = None
+    m = re.search('REG_SZ\\s+([^\\r\\n]+)', out.decode('utf8'))
+
+    if m:
+        engine_path = m.group(1)
+
+
+    logging.info(out)
+    logging.info(engine_path)
+
+main()
+
+'''def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    curprj.targetName = sys.argv[1]
+    curprj.engineDir = sys.argv[3]
+    curprj.prjDir = sys.argv[2]
+
+    curprj.prjName = curprj.targetName.replace('Editor', '')
+    logging.info("prjName=" + curprj.prjName + " targetName=" + curprj.targetName + " engineDir=" + curprj.engineDir + " prjFile=" + curprj.prjDir)
+
+    def fixSourceFilesIn(dir):
+        for fn in os.listdir(dir):
+            fullName = os.path.join(dir, fn)
+
+            if os.path.isdir(fullName):
+                fixSourceFilesIn(fullName)
+
+            if fullName.endswith('.cpp'):
+                headergen.generateHeaderForCppFile(fullName)
+
+    fixSourceFilesIn(os.path.join(curprj.prjDir, 'Source'))
+
+    logging.info("prebuild complete")
+
+main()'''

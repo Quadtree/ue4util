bbea3b1f1eb2829ae8ede0d09d9ecba7c31e21db
Quadtree <quadtree@gmail.com>
2018-08-25 18:02:20 -0700

    Cleanup

--- watch.py
+++ watch.py
@@ -73,29 +73,3 @@ def main():
         time.sleep(0.5)
 
 main()
-
-'''def main():
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    curprj.targetName = sys.argv[1]
-    curprj.engineDir = sys.argv[3]
-    curprj.prjDir = sys.argv[2]
-
-    curprj.prjName = curprj.targetName.replace('Editor', '')
-    logging.info("prjName=" + curprj.prjName + " targetName=" + curprj.targetName + " engineDir=" + curprj.engineDir + " prjFile=" + curprj.prjDir)
-
-    def fixSourceFilesIn(dir):
-        for fn in os.listdir(dir):
-            fullName = os.path.join(dir, fn)
-
-            if os.path.isdir(fullName):
-                fixSourceFilesIn(fullName)
-
-            if fullName.endswith('.cpp'):
-                headergen.generateHeaderForCppFile(fullName)
-
-    fixSourceFilesIn(os.path.join(curprj.prjDir, 'Source'))
-
-    logging.info("prebuild complete")
-
-main()'''
